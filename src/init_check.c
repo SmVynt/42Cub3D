3594588a09d4eae4b7a48d9c1f6745f430a15dc6
SmVynt <vyntershtoff@mail.ru>
2025-09-29 11:38:12 +0200

    adsasd

--- src/init_check.c
+++ src/init_check.c
@@ -6,7 +6,7 @@
 /*   By: psmolin <psmolin@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 13:45:35 by psmolin           #+#    #+#             */
-/*   Updated: 2025/09/26 17:17:44 by psmolin          ###   ########.fr       */
+/*   Updated: 2025/09/29 11:37:55 by psmolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,10 @@ static void ft_checkfile(const char *src)
 static void ft_checkarguments(int argc, char **argv)
 {
 	if (argc != 2 || ft_strlen(argv[1]) < 5
-		|| ft_strncmp(argv[1] + ft_strlen(argv[1]) - 4, ".ber", 4) != 0)
+		|| ft_strncmp(argv[1] + ft_strlen(argv[1]) - 4, ".cub", 4) != 0)
 	{
 		ft_exit("Please run the game with this command:\
-			\n./cub3d maps/<mapname.ber>\n");
+			\n./cub3d maps/<mapname.cub>\n");
 	}
 }
 
