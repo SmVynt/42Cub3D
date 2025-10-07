59c573430ebb88531ee9bab3ee90dc0148aadf36
SmVynt <vyntershtoff@mail.ru>
2025-10-07 18:04:22 +0200

    correct direction

--- src/init_game.c
+++ src/init_game.c
@@ -6,7 +6,7 @@
 /*   By: psmolin <psmolin@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/26 13:45:35 by psmolin           #+#    #+#             */
-/*   Updated: 2025/10/07 18:01:37 by psmolin          ###   ########.fr       */
+/*   Updated: 2025/10/07 18:04:06 by psmolin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,11 +23,11 @@ static void	ft_set_player(void)
 	player->pos = (t_vec2){(float)game->map.start.u + 1.0f, (float)game->map.start.v + 1.0f};
 	start_char = game->map.tile[game->map.start.v][game->map.start.u];
 	if (start_char == 'N')
-		player->lookdir = (t_vec3){0.0f, 1.0f, 0.0f};
+		player->lookdir = (t_vec3){0.0f, -1.0f, 0.0f};
 	if (start_char == 'E')
 		player->lookdir = (t_vec3){1.0f, 0.0f, 0.0f};
 	if (start_char == 'S')
-		player->lookdir = (t_vec3){0.0f, -1.0f, 0.0f};
+		player->lookdir = (t_vec3){0.0f, 1.0f, 0.0f};
 	if (start_char == 'W')
 		player->lookdir = (t_vec3){-1.0f, 0.0f, 0.0f};
 }
